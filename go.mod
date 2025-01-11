572884760d00cf7d91705c07a8e631a644d7560b
shafnybuilds <shafnymhd@proton.me>
2025-01-12 00:56:37 +0530

    second

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module example.com/mouse-counter
 
 go 1.23.4
+
+require github.com/a-h/templ v0.3.819 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"example.com/mouse-counter/views"
+	"github.com/a-h/templ"
+	"net/http"
+)
+
 func main() {
-	
+	component := views.Hello("World")
+
+	http.Handle("/", templ.Handler(component))
+
+	http.ListenAndServe(":8080", nil)
 }
