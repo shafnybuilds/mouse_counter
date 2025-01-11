2265570ffe70bca62c31330f8c8074f885bc2c0b
shafnybuilds <shafnymhd@proton.me>
2025-01-12 01:06:58 +0530

    third_commit

--- main.go
+++ main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"net/http"
+
 	"example.com/mouse-counter/views"
 	"github.com/a-h/templ"
-	"net/http"
+	"github.com/labstack/echo/v4"
 )
 
+func RenderComponent(c echo.Context, status int, cmp templ.Component) error {
+	c.Response().WriteHeader(status)
+	c.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTML)
+
+	return cmp.Render(c.Request().Context(), c.Response().Writer)
+}
+
 func main() {
-	component := views.Hello("World")
+	e := echo.New()
 
-	http.Handle("/", templ.Handler(component))
+	e.GET("/", func(c echo.Context) error {
+		return RenderComponent(c, http.StatusOK, views.Hello("World"))
+	})
 
-	http.ListenAndServe(":8080", nil)
+	e.Logger.Fatal(e.Start(":8080"))
 }
